01194628be597743b99846d6d34daf6a3918415b
Sergey Kovalev <dipdeepcode@mail.ru>
2023-06-13 19:02:49 +0300

    Рефакторинг

--- src/main/java/ru/ddc/ss2/SSSMApplication.java
+++ src/main/java/ru/ddc/ss2/SSSMApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ss2Application {
+public class SSSMApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ss2Application.class, args);
+		SpringApplication.run(SSSMApplication.class, args);
 	}
 
 }
